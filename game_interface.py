0860133af369108d4ba4fea5b3d77f0d96bd2ba9
Alfaxad <alfaxadeyembe@gmail.com>
2022-01-30 01:11:32 +0300

    Changed button colors

--- game_interface.py
+++ game_interface.py
@@ -199,22 +199,22 @@ def play():
 	wpc = partial(withpc, menu)
 	wpl = partial(withplayer, menu)
 	
-	head = Button(menu, text = "---Welcome to tic-tac-toe---",
-				activeforeground = 'red',
-				activebackground = "yellow", bg = "red",
-				fg = "yellow", width = 500, font = 'summer', bd = 5)
+	head = Button(menu, text = "Welcome to Quantum tic-tac-to",
+				activeforeground = 'white',
+				activebackground = "blue", bg = "green",
+				fg = "white", width = 500, font = 'summer', bd = 5)
 	
 	B1 = Button(menu, text = "Single Player", command = wpc,
-				activeforeground = 'red',
-				activebackground = "yellow", bg = "red",
-				fg = "yellow", width = 500, font = 'summer', bd = 5)
+				activeforeground = 'white',
+				activebackground = "blue", bg = "green",
+				fg = "white", width = 500, font = 'summer', bd = 5)
 	
-	B2 = Button(menu, text = "Multi Player", command = wpl, activeforeground = 'red',
-				activebackground = "yellow", bg = "red", fg = "yellow",
+	B2 = Button(menu, text = "Multi Player", command = wpl, activeforeground = 'white',
+				activebackground = "blue", bg = "green", fg = "white",
 				width = 500, font = 'summer', bd = 5)
 	
-	B3 = Button(menu, text = "Exit", command = menu.quit, activeforeground = 'red',
-				activebackground = "yellow", bg = "red", fg = "yellow",
+	B3 = Button(menu, text = "Exit", command = menu.quit, activeforeground = 'white',
+				activebackground = "blue", bg = "green", fg = "white",
 				width = 500, font = 'summer', bd = 5)
 	head.pack(side = 'top')
 	B1.pack(side = 'top')
